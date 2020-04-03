a2dc8ceaa08b642fe1a4bca14e35fa65cad39d71
Spencer Harston <spencer314@gmail.com>
2020-04-03 15:53:35 -0600

    Refactor code to function calls

--- xkcd-enhancer/script.js
+++ xkcd-enhancer/script.js
@@ -1,9 +1,15 @@
 console.log('[xkcd enhancer js loaded]');
 
-// display the comic's title attribute below the comic
-var comicDiv = document.getElementById('comic');
-var comicTitle = document.querySelectorAll('#comic img')[0].title;
-var titleElement = document.createElement('p');
-titleElement.setAttribute('id', 'comicTitle');
-titleElement.innerHTML = comicTitle;
-comicDiv.appendChild(titleElement);
+// function definitions
+// display the comic's hover text (title attribute) below the comic
+function displayComicHoverText(){
+    var comicDiv = document.getElementById('comic');
+    var comicHoverText = document.querySelectorAll('#comic img')[0].title;
+    var hoverTextElement = document.createElement('p');
+    hoverTextElement.setAttribute('id', 'hoverText');
+    hoverTextElement.innerHTML = comicHoverText;
+    comicDiv.appendChild(hoverTextElement);    
+}
+
+// call functions
+displayComicHoverText();
