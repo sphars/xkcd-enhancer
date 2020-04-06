5c5fa99fbe5491ac58a25d23bd37d4048667dbcb
Spencer Harston <spencer314@gmail.com>
2020-04-05 21:03:45 -0600

    Add explainxckd link to comic navigation

--- xkcd-enhancer/script.js
+++ xkcd-enhancer/script.js
@@ -33,6 +33,7 @@ async function getComicJson(url){
 // process the comic data
 function processComicJson(json){
     displayComicHoverText(json.alt);
+    addExplainLink(json.num);
 }
 
 // display the comic's hover text (title attribute) below the comic
@@ -43,6 +44,26 @@ function displayComicHoverText(hoverText){
     hoverTextElement.setAttribute('id', 'hoverText');
     hoverTextElement.appendChild(document.createTextNode(hoverText));
     comic.parentNode.insertBefore(hoverTextElement, comic.nextSibling);    
+}
+
+// add an explain link to the comic navigation
+function addExplainLink(comicNumber){
+    var explainURL = `https://www.explainxkcd.com/${comicNumber}/`;
+
+    var explainLink = document.createElement('a');
+    explainLink.href = explainURL;
+    explainLink.target = "_blank";
+    explainLink.accessKey = 'e';
+    explainLink.innerText = "Explain";
+    
+    var explainNav = document.createElement('li');
+    explainNav.append(explainLink);
+
+    var navs = document.querySelectorAll('.comicNav');
+
+    for (var nav of navs) {
+        nav.insertBefore(explainNav.cloneNode(true), nav.children[3]);
+    }
 }
 
 // call functions
